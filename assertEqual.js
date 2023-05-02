bfea9deeb1fe56cdc3bb8253cfe4a4cd6e2eccc5
Yuze Wang <yuzewang621@gmail.com>
2023-05-01 21:56:55 -0600

    create my own assertion function comparing 2 values

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("\u{1F600} Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("\u{1F602} Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Pistaches", "Pistaches");
+assertEqual("I am strong", "Iam strong");
+assertEqual(111,56);
+assertEqual(121, "121");
