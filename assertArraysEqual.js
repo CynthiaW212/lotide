af153e8d6cfb868d3a05229321ba8dda67694633
Yuze Wang <yuzewang621@gmail.com>
2023-05-06 11:28:36 -0600

    update for refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,8 @@
-// Function eqArrays is used to compare two arrays and determine if they are equal. If the two arrays are equal, the function returns true. If the arrays are not equal, the function returns false.
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    let i = 0;
-    while (i < array1.length) {
-      if (array1[i] === array2[i]) {
-        i++;
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-// Function assertArrayEqual is used to compare two arrays and determine if they are equal and console.log an appropriate message to the console.
+/*Function assertArrayEqual is used to compare two arrays and determine if they are equal and console.log an appropriate message to the console.
+Here we don't consider about comparing  nested arrays
+*/
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1,array2)) {
     console.log(array1, "and", array2, "are equal!");
@@ -25,7 +12,4 @@ const assertArrayEqual = function(array1, array2) {
 
 };
 
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArrayEqual;
