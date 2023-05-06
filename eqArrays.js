dcb6be07811064320901502937397d12e511ef32
Yuze Wang <yuzewang621@gmail.com>
2023-05-06 10:56:16 -0600

    refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,20 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`\u{1F600} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F602} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
 
   if (array1.length !== array2.length) {
     return false;
-  } else {
-    let i = 0;
-    while (i < array1.length) {
-      if (array1[i] === array2[i]) {
-        i++;
-      } else {
-        return false;
-      }
-    }
-    return true;
   }
+  let i = 0;
+  while (i < array1.length) {
+
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+
+    i++;
+  }
+  return true;
+  
 
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+module.exports = eqArrays;
