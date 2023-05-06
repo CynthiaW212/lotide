0060d1fab86f6393985fa1227700f184f5fb1ca2
Yuze Wang <yuzewang621@gmail.com>
2023-05-06 10:07:07 -0600

    update - refactor

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-// FUNCTION IMPLEMENTATION: compare two inputs
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`\u{1F600} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F602} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual');
 
 //FUNCTION IMPLEMENTATION: Get the head of an array
 const head = function(array) {
@@ -14,7 +6,4 @@ const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
