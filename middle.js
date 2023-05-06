21ddc05de0cda5ed436d6c894c5ad2ea5baa4a34
Yuze Wang <yuzewang621@gmail.com>
2023-05-06 12:00:42 -0600

    update for refactor

--- middle.js
+++ middle.js
@@ -1,33 +1,5 @@
-// Function eqArrays is used to compare two arrays and determine if they are equal. If the two arrays are equal, the function returns true. If the arrays are not equal, the function returns false.
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    // console.log("length is not same");
-    return false;
-  } else {
-    let i = 0;
-    while (i < array1.length) {
-      if (array1[i] === array2[i]) {
-        i++;
-      } else {
-        console.log(i);
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-/*Function assertArrayEqual is used to compare two arrays and determine if they are equal and console.log an appropriate message to the console.
-Here we don't consider about comparing  nested arrays
-*/
-const assertArrayEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(array1, "and", array2, "are equal!");
-  } else {
-    console.log(array1, "and", array2, "are not equal!");
-  }
-
-};
 
 // ACTUAL FUNCTION
 const middle = function(array) {
@@ -48,8 +20,5 @@ const middle = function(array) {
   return midArray;
 };
 
-//Test code
-assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
-assertArrayEqual(middle([1, 2]),[]);
-assertArrayEqual(middle([1]),[]);
+module.exports = middle;
+
