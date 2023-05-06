33c3a6ca422ebb8b548750eb7b2140d43c7d05c4
Yuze Wang <yuzewang621@gmail.com>
2023-05-06 10:08:27 -0600

    update - refactor

--- assertEqual.js
+++ assertEqual.js
@@ -1,17 +1,20 @@
+
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("\u{1F600} Assertion Passed: " + actual + " === " + expected);
+    console.log(`\u{1F600} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\u{1F602} Assertion Failed: " + actual + " !== " + expected);
+    console.log(`\u{1F602} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Pistaches", "Pistaches");
-assertEqual("I am strong", "Iam strong");
-assertEqual(111,56);
-assertEqual(121, "121");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("Pistaches", "Pistaches");
+// assertEqual("I am strong", "Iam strong");
+// assertEqual(111,56);
+// assertEqual(121, "121");
+
+module.exports = assertEqual;
