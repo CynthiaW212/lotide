d7de353e606ca26289a0a22a8d62963389b59aea
Yuze Wang <yuzewang621@gmail.com>
2023-05-06 18:40:36 -0600

    exports functions document

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const findKey = require('./findKey');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {head, tail, middle, findKey, countLetters,countOnly, findKeyByValue,flatten,letterPositions,map,takeUntil,without};
